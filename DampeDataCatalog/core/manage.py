05b7f334844ccbc8324d6541d9eced2ebf7d973e
zimmer <zimmer@fysik.su.se>
2016-09-07 00:37:44 +0200

    fixes.

--- DampeDataCatalog/core/manage.py
+++ DampeDataCatalog/core/manage.py
@@ -6,16 +6,17 @@ sys_path.append(abspath(oPjoin(dirname(__file__), '..')))
 from flask.ext.script import Manager, Server, Shell
 import DampeDataCatalog.core.models as DFCModels
 from DampeDataCatalog.core import app, db
+from DampeDataCatalog.config.defaults import cfg
+
 
 def _make_context():
     return dict(app=app, db=db, models=DFCModels)
-
 
 manager = Manager(app)
 # Turn on debugger by default and reloader
 manager.add_command("runserver", Server(use_debugger=True,
                                         use_reloader=True,
-                                        host="127.0.0.1:5000"))
+                                        host="0.0.0.0:5000"))
 
 manager.add_command("shell", Shell(make_context=_make_context))
 
