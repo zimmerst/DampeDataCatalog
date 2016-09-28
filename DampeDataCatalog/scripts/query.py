bbcabe6bab0f46fcbf6b3990c6df171325d32caf
zimmer <zimmer@fysik.su.se>
2016-09-28 15:28:27 +0200

    couple more fixes.

--- DampeDataCatalog/scripts/query.py
+++ DampeDataCatalog/scripts/query.py
@@ -12,19 +12,19 @@ URL = "http://dampevm6.unige.ch:4000"
             
 def main(args=None):
     parser = ArgumentParser(usage="Usage: %(prog)s [options]",description="query entries in DFC")
-    #for key in ['site','fileName','dataset','kind']:
-    #    parser.add_argument("-%s"%key[0],"--%s"%key, dest=key, type=str, default=None)
-    parser.add_argument("-k","--kind",dest='kind', type=str, default=None, help="MC or 2A or similar")
-    parser.add_argument("-d","--dataset",dest='dataset', type=str, default=None, help="name of top-level dataset")
-    parser.add_argument("-f","--fileName",dest='fileName', type=str, default=None, help="query a specific fileName (this may take some time to complete)")
-    parser.add_argument("-s","--site",dest='site', type=str, default=None, help="site you want to get replicas for")
+    for key in ['site','fileName','dataset','kind']:
+        parser.add_argument("-%s"%key[0],"--%s"%key, dest=key, type=str, default=None)
+    #parser.add_argument("-k","--kind",dest='kind', type=str, default=None, help="MC or 2A or similar")
+    #parser.add_argument("-d","--dataset",dest='dataset', type=str, default=None, help="name of top-level dataset")
+    #parser.add_argument("-f","--fileName",dest='fileName', type=str, default=None, help="query a specific fileName (this may take some time to complete)")
+    #parser.add_argument("-s","--site",dest='site', type=str, default=None, help="site you want to get replicas for")
     parser.add_argument("--fileType",dest='fileType', type=str, default=None, help="file extension to query (root/fits)")
     parser.add_argument("--status",dest='status', type=str, default="good", help="return files matching this status")
     # time selection
     parser.add_argument("--tStart",dest='tStart', type=long, default=None, help="tStart MJD")
     parser.add_argument("--tStop",dest='tStop', type=long, default=None, help="tStart MJD")
-    parser.add_argument("--tStartDT",dest='tStartDT', type=str, default=None, help="tStart in UTZ:, format is %Y-%m-%d %H:%M:%S.%f")
-    parser.add_argument("--tStopDT",dest='tStopDT', type=str, default=None, help="tStart in UTZ:, format is %Y-%m-%d %H:%M:%S.%f")
+    parser.add_argument("--tStartDT",dest='tStartDT', type=str, default=None, help="tStart in UTZ:, format is YYYY-mm-dd HH:MM:SS.f")
+    parser.add_argument("--tStopDT",dest='tStopDT', type=str, default=None, help="tStart in UTZ:, format is YY-mm-dd HH:MM:SS.f")
     parser.add_argument("-o","--output",dest='output',type=str,default=None,help='use this option to redirect output to file')
     opts = parser.parse_args(args)
     my_dict = vars(opts)
